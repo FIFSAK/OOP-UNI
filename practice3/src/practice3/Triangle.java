15a8da595f016f217d9bd16e560b743b3dcc3cfa
FIFSAK <anuar200572@gmail.com>
2023-09-18 22:46:39 +0600

    task 2

--- practice3/src/practice3/Triangle.java
+++ practice3/src/practice3/Triangle.java
@@ -1,5 +1,21 @@
 package practice3;
 
 public class Triangle {
-
+	int width;
+	
+	public Triangle(int width) {
+		this.width = width;
+	}
+	
+	public String toString() {
+		String symbol = "[*]";
+		String result = "";
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j<i; j++) {
+				result += symbol;
+			}
+			result += "\n";
+		}
+		return result;
+	}
 }
