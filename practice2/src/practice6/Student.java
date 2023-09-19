9b7803ccda28e0c4ed947a401d96876832926912
FIFSAK <anuar200572@gmail.com>
2023-09-19 13:10:56 +0600

    testing book examples

---
+++ practice2/src/practice6/Student.java
@@ -0,0 +1,17 @@
+package practice6;
+
+public class Student {
+	private String name;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void method1 (Student a) {
+		a.setName("Aray");
+		} 
+	public void method2 ( ) {
+		Student b = new Student();
+		b.setName("Adiya");
+		method1(b); // now s is ?
+		System.out.println(b.name);
+		}
+}
--- practice2/src/practice6/main.java
+++ practice2/src/practice6/main.java
@@ -3,8 +3,10 @@ package practice6;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("asjdasdasdasdasdada");
-
+		Student a = new Student();
+		a.method2();
+		
 	}
 
 }
+ 	
