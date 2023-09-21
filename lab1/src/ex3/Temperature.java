4453f869ca8dbd346ba738d0a33d971d86781ca1
FIFSAK <anuar200572@gmail.com>
2023-09-21 14:11:12 +0600

    ex3 half

---
+++ lab1/src/ex3/Temperature.java
@@ -0,0 +1,20 @@
+package ex3;
+
+public class Temperature {
+	private double val;
+	private char scale;
+	
+	public void setVal(double val) {
+		this.val = val;
+	}
+	public void setScale(char scale) {
+		this.scale = scale;
+	}
+	public double getVal() {
+		return val;
+	}
+	public char getScale(char scale) {
+		return scale;
+	}
+	
+}
