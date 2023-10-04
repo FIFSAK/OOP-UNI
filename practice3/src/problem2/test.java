d9e12cc9e691c773dd67c4512993d543212b6078
FIFSAK <anuar200572@gmail.com>
2023-10-04 23:38:15 +0600

    some change

--- practice3/src/problem2/test.java
+++ practice3/src/problem2/test.java
@@ -1,10 +1,27 @@
 package problem2;
-
+import java.util.Scanner;
+import java.util.Vector;
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		Vector <Person> persons = new Vector<Person>();
+		Person p = new Person("A", "123");
+		persons.add(p);
+		Student s = new Student("S", "333", "it", 1, 2.5);
+		persons.add(s);
+		Staff st = new Staff("ST", "222", "93", 4.2);
+		persons.add(st);
+		System.out.println(persons);
+//		System.out.println("inset 1 add new 2");
+//		int temp = input.nextInt();
+//		if(temp==1) {
+//			
+//		}
+//		if(temp==2) {
+//			
+//		}
 	}
 
 }
