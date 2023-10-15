ef37b64b2fc378f30dec8b1c0ac83e50d6677e03
FIFSAK <anuar200572@gmail.com>
2023-10-15 23:04:43 +0600

    Update DVD.java

--- lab2/src/ex1/DVD.java
+++ lab2/src/ex1/DVD.java
@@ -1,5 +1,12 @@
 package ex1;
 
-public class DVD {
-
+public class DVD extends LibraryItem {
+	int memory;
+	
+	public DVD() {}
+	
+	public DVD(String title, String author, int publicatonYear, int memory) {
+		super(title, author, publicatonYear);
+		this.memory = memory;
+	}
 }
