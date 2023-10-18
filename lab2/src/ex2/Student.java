bc36fa24011853b506072842813310195f147b29
FIFSAK <anuar200572@gmail.com>
2023-10-18 22:29:25 +0600

    Update Student.java

--- lab2/src/ex2/Student.java
+++ lab2/src/ex2/Student.java
@@ -1,5 +1,33 @@
 package ex2;
 
-public class Student {
+import java.util.Objects;
 
+public class Student extends Person {
+	String uni;
+
+	@Override
+	public String toString() {
+		return "Student [uni=" + uni + super.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(uni);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(uni, other.uni);
+	}
+	
 }
