d3a235935e481553587c620df1ecaf07254ff8f9
FIFSAK <anuar200572@gmail.com>
2023-11-01 23:50:33 +0600

    1 and 2

--- practice5/src/practice5/Example1.java
+++ practice5/src/practice5/Example1.java
@@ -1,18 +1,20 @@
 package practice5;
 
-public class Example1
-{
-    public static void main(String[] args)
-    {
-	int     denominator, numerator, ratio;
-
-
-	numerator   = 5;
-	denominator = 2;
-
-    ratio = numerator / denominator;
-	System.out.println("The answer is: "+ratio);
-
-	System.out.println("Done."); // Don't move this line
+public class Example1{
+    public static void main(String[] args){
+		int denominator, numerator, ratio;
+	
+		numerator   = 5;
+		denominator = 0;
+		try {		
+			ratio = numerator / denominator;
+			System.out.println("The answer is: "+ratio);
+			System.out.println("Done."); // Don't move this line
+		}
+		catch (java.lang.ArithmeticException e) {
+			System.out.println("Divide by 0.");
+			printStackTrace();
+		}
+		
     }
 }
