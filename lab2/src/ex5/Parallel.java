f4f908c5f06e8fe4f5ab0b00b98313bf5d750e7f
FIFSAK <anuar200572@gmail.com>
2023-11-02 15:14:20 +0600

    tipa tested 5

--- lab2/src/ex5/Parallel.java
+++ lab2/src/ex5/Parallel.java
@@ -1,9 +1,12 @@
 package ex5;
 
 public class Parallel extends Resistor {
-	double r;
 	public Parallel(int r1, int r2) {
 		r = 1/(1/r1+1/r2);
+	}
+	public Parallel(Circuit c, Circuit d) {
+		// TODO Auto-generated constructor stub
+		r = 1/(1/c.getResistance() + 1/d.getResistance());
 	}
 	@Override
 	public double getResistance() {
---
+++ lab2/src/ex5/test.java
@@ -0,0 +1,23 @@
+package ex5;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circuit a = new Resistor(3);
+		Circuit b = new Resistor(3);
+		Circuit c = new Resistor(3);
+		Circuit d = new Resistor(3);
+		Circuit e = new Resistor(2);
+		Circuit f = new Series(a, b);
+		Circuit g = new Parallel(c, d);
+		Circuit h = new Series(g, e);
+		Circuit circuit = new Parallel(h, f);
+		double R = circuit.getResistance();
+		double V = 6.0;
+		double I = V / R;
+		System.out.println("Total Resistance in the Parallel Circuit: " + R + " Ohms");
+		System.out.println("Current in the Circuit: " + I + " Amperes");
+	}
+
+}
