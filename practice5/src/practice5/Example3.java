c47a907bcb002264f450d4692b07fed42487b21d
FIFSAK <anuar200572@gmail.com>
2023-11-02 11:39:17 +0600

    finish 5

--- practice5/src/practice5/Example3.java
+++ practice5/src/practice5/Example3.java
@@ -7,17 +7,10 @@ public class Example3
 	int              i;
 	int[]            data = {50, 320, 97, 12, 2000};
 
-
-	try 
-        {
-	    for (i=0; i < 10; i++)
+	    for (i=0; i < data.length; i++)
             {
 		System.out.println(data[i]);
 	    }
-	} 
-        catch (ArrayIndexOutOfBoundsException aioobe)
-        {
 	    System.out.println("Done");
-	}
     }
 }
